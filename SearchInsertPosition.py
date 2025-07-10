3444aa4f981fdf866eeb7f148da130a5e099a88d
Haniya Khan <151783908+Haniyakhannn@users.noreply.github.com>
2025-07-09 17:02:42 -0700

    Add files via upload

---
+++ SearchInsertPosition.py
@@ -0,0 +1,17 @@
+def searchInsert(self, nums, target):
+        low=0
+        high=len(nums)-1
+        while low <= high:
+
+            mid = low + (high - low)//2
+
+            if target == nums[mid]:
+                return mid
+
+            elif target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
+        
